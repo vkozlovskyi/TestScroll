4ed9c2db08df670e1b170fd3cc6ef407b6127a7a
Vladimir Kozlovskyi <vlad.kozlovskyi@gmail.com>
2022-07-04 15:37:23 +0300

    Adding cells implemented

--- TestScroll/ViewController.swift
+++ TestScroll/ViewController.swift
@@ -7,9 +7,22 @@
 
 import UIKit
 
+struct Cell {
+  
+}
+
 class ViewController: UIViewController {
+  
+  var array: [Cell] = []
 
   @IBOutlet weak var collectionView: UICollectionView!
+  
+  
+  @IBAction func didPress(_ sender: Any) {
+    array.append(Cell())
+    collectionView.reloadData()
+    collectionView.scrollToItem(at: IndexPath(row: array.count-1, section: 0), at: .right, animated: true)
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,7 +40,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
   }
   
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    10
+    array.count
   }
   
   
